119c81a7ec5ebfbcb3f5b84b2cbc0719bf8bed46
Davisampaiom <71107984+Davisampaiom@users.noreply.github.com>
2021-01-18 14:50:39 -0300

    Create index.js
    revisao3.1

---
+++ src/components/pages/espaco_seguro/index.js
@@ -0,0 +1,13 @@
+import "./noticias.css";
+
+function Espaco_seguro() {
+  return (
+    <>
+      <div className="container-fluid mainPage">
+       <h1>espaço seguro<h1>
+      </div>
+    </>
+  );
+}
+
+export default Espaco_seguro;
